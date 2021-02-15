6b779a336cb15759b32fbf46c2cba777ef8d8baf
HARSH PARMAR <60259582+Harshparmar105@users.noreply.github.com>
2021-02-16 04:38:55 +0530

    Add files via upload

---
+++ video.py
@@ -0,0 +1,23 @@
+import cv2 , time
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+video = cv2.VideoCapture(0)
+a = 1
+
+while(True):
+    a = a+1
+    check , frame = video.read()
+    print(frame)
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+    for x, y, w, h in faces:
+        gray = cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imshow('capturing',gray)
+    key = cv2.waitKey(1)
+    if(key == ord('q')):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
